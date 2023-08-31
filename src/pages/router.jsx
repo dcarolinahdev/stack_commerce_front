08790249bdc166c1e0dd4f1456410066b3ce7073
santiagogutierre8 <santiagogutierre8@gmail.com>
2023-08-31 09:48:11 -0500

    se suben vistas de register, singln y rutas

--- src/pages/router.jsx
+++ src/pages/router.jsx
@@ -1,6 +1,8 @@
 import { createBrowserRouter, redirect } from "react-router-dom";
 import Main from "../layouts/Main";
 import Index from "./Index";
+import Singln from "../pages/Singln.jsx";
+import Resgister from "../pages/Resgister.jsx"
 
 const router = createBrowserRouter([
     {
@@ -10,7 +12,8 @@ const router = createBrowserRouter([
             {path:"/", element:<Index/>},
             { path: "/index", element: <Index /> },
             { path: "/home", element: <Index /> },
-
+            { path: '/register', element: <Resgister />},
+            { path: '/singin', element: <Singln/> },
         ]
     }
 ])
