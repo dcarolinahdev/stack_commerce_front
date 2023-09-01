816a2586d00962eb32cd93298ce272711fa6bb02
harold alzate <haroldalzate99@gmail.com>
2023-08-31 20:11:00 -0500

    Category view login 	new file:   src/pages/Category_login.jsx 	modified:   src/pages/router.jsx

--- src/pages/router.jsx
+++ src/pages/router.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter, redirect } from "react-router-dom";
 import Main from "../layouts/Main";
 import Index from "./Index";
 import Cart from "./Cart";
+import Category_login from "./Category_login";
 
 const router = createBrowserRouter([
     {
@@ -12,6 +13,7 @@ const router = createBrowserRouter([
             { path: "/index", element: <Index /> },
             { path: "/home", element: <Index /> },
             { path: "/cart", element: <Cart /> },
+            { path: "/category_login", element: <Category_login /> },
 
         ]
     }
