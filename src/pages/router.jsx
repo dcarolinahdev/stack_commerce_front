db17b54e2389b442b60a17249374518a2dd467c4
santiagogutierre8 <santiagogutierre8@gmail.com>
2023-08-31 22:17:04 -0500

    notallowed

--- src/pages/router.jsx
+++ src/pages/router.jsx
@@ -1,8 +1,9 @@
 import { createBrowserRouter, redirect } from "react-router-dom";
 import Main from "../layouts/Main";
 import Index from "./Index";
-import Singln from "../pages/Singln.jsx";
-import Resgister from "../pages/Resgister.jsx"
+import SignIn from "../pages/Signln"
+import Register from "./Register.jsx"
+import NotAllowed from "./NotAllowed";
 
 const router = createBrowserRouter([
     {
@@ -12,8 +13,9 @@ const router = createBrowserRouter([
             {path:"/", element:<Index/>},
             { path: "/index", element: <Index /> },
             { path: "/home", element: <Index /> },
-            { path: '/register', element: <Resgister />},
-            { path: '/singin', element: <Singln/> },
+            { path: '/register', element: <Register />},
+            { path: '/signIn', element: <SignIn/> },
+            {path:"/not-allowed",element: <NotAllowed />},
         ]
     }
 ])
