4466897263e8add3a8b83408ab7ea0704d8950c1
Diana Carolina Hernandez Abahonza <dianacaro3552@gmail.com>
2023-09-04 16:58:36 -0500

    Adding section payment

---
+++ src/components/Payment.jsx
@@ -0,0 +1,26 @@
+import { AiFillCreditCard } from "react-icons/ai"
+import { GiGreekTemple } from "react-icons/gi"
+import { BsCashCoin } from "react-icons/bs"
+
+export default function Payment() {
+  return (
+    <div className="flex py-4 px-6 justify-between items-center text-t_background bg-[#EDEDED] mb-4">
+
+        <div className="flex items-center">
+            <AiFillCreditCard className="w-10 h-10" />
+            <span className="text-[14px] pl-2 uppercase">48 horas</span>
+        </div>
+
+        <div className="flex items-center">
+            <GiGreekTemple className="w-10 h-10" />
+            <span className="text-[14px] pl-2 uppercase">wire transfer</span>
+        </div>
+
+        <div className="flex items-center">
+            <BsCashCoin className="w-10 h-10" />
+            <span className="text-[14px] pl-2 uppercase">cash payment</span>
+        </div>
+
+    </div>
+  )
+}
--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import ProductList from "../components/ProductsList/ProductsList";
 import Carousel from "../components/Carousel/Carousel";
+import Payment from "../components/Payment";
 
 const Index = () => {
   return (
     <div className="">
       <Carousel />
+      <Payment />
       <ProductList />
     </div>
   );
