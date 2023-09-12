b6e1ada1951301b29837dff8783085cfb70a2f4a
harold alzate <haroldalzate99@gmail.com>
2023-09-12 18:52:27 -0500

    modified:   src/components/Navbar.jsx 	modified:   src/pages/Admin.jsx

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -3,75 +3,150 @@ import React from "react";
 const Admin = () => {
   return (
     <>
-      <section className="bg-center bg-no-repeat bg-[url('https://www.esic.edu/sites/default/files/2020-09/optimizar-tu-ecommerce.jpg')] bg-[#7f5af0] bg-blend-multiply">
-        <div className="px-4 mx-auto max-w-screen-xl text-center py-24 lg:py-56">
-          <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-white md:text-5xl lg:text-6xl">
-            ADMIN PANEL
-          </h1>
-          <p className="mb-8 text-lg font-normal text-gray-300 lg:text-xl sm:px-16 lg:px-48">
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deserunt
-            commodi dicta, autem possimus earum in optio sequi veniam, fugiat
-            at, officiis aut ad? Magni animi laborum facilis, quod sequi
-            pariatur.
-          </p>
-          <div className="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4"></div>
-        </div>
-      </section>
+<div className="bg-blue-500 py-16 text-white text-center mt-8">
+  <div className="container mx-auto">
+    <h1 className="text-4xl font-bold">Admin Panel</h1>
+    <p className="text-xl mt-4">Welcome to your personal area!!</p>
+  </div>
+</div>
 
-      <div className="overflow-x-auto m-10">
-        <table className="w-full text-sm text-left text-gray-800 bg-white border dark:text-gray-300 dark:bg-gray-800 dark:border-gray-700">
-          <thead className="text-xs bg-gray-100 dark:bg-gray-700">
-            <tr>
-              <th
-                scope="col"
-                className="px-6 py-3 font-semibold text-3xl text-gray-900 dark:text-white text-center"
+<div className="container mx-auto mt-8 p-4">
+  <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4  text-white">
+    <div className="bg-yellow-500 p-4 rounded shadow-md">
+      <h2 className="text-xl font-semibold">Pending Orders</h2>
+      <p className="text-3xl font-bold mt-2">25</p>
+    </div>
+
+    <div className="bg-green-500 p-4 rounded shadow-md">
+      <h2 className="text-xl font-semibold">Completed Orders</h2>
+      <p className="text-3xl font-bold mt-2">50</p>
+    </div>
+
+    <div className="bg-pink-500 p-4 rounded shadow-md">
+      <h2 className="text-xl font-semibold">Users</h2>
+      <p className="text-3xl font-bold mt-2">1000</p>
+    </div>
+
+    <div className="bg-blue-500 p-4 rounded shadow-md">
+      <h2 className="text-xl font-semibold">Total Sold</h2>
+      <p className="text-3xl font-bold mt-2">$10,000</p>
+    </div>
+  </div>
+
+  <div className="mt-8 bg-white rounded shadow-md overflow-x-auto">
+    <div className="flex w-full auto"></div>
+    <table className="w-full table-auto">
+      <thead>
+        <tr className="bg-blue-500 text-white">
+          <th className="px-4 py-2">Order</th>
+          <th className="px-4 py-2">Date</th>
+          <th className="px-4 py-2">Customer</th>
+          <th className="px-4 py-2">Total</th>
+          <th className="px-4 py-2">Status</th>
+          <th className="px-4 py-2">Actions</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td className="border px-4 py-2">1</td>
+          <td className="border px-4 py-2">2023-09-12</td>
+          <td className="border px-4 py-2">John Doe</td>
+          <td className="border px-4 py-2">$100</td>
+          <td className="border px-4 py-2">
+            <span className="px-2 py-1 rounded bg-yellow-500 text-white">
+              Pending
+            </span>
+          </td>
+          <td className="border px-4 py-2">
+            <a href="#" className="text-blue-500 hover:text-blue-700 mr-2">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                className="w-5 h-5"
               >
-                Product
-              </th>
-              <th
-                scope="col"
-                className="px-6 py-3 font-semibold text-3xl text-gray-900 dark:text-white text-center"
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+                />
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M15 5a6 6 0 00-6 6v2a6 6 0 006 6 6 6 0 006-6v-2a6 6 0 00-6-6z"
+                />
+              </svg>
+            </a>
+            <a href="#" className="text-red-500 hover:text-red-700">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                className="w-5 h-5"
               >
-                ID
-              </th>
-              <th
-                scope="col"
-                className="px-6 py-3 font-semibold text-3xl text-gray-900 dark:text-white text-center"
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              </svg>
+            </a>
+          </td>
+        </tr>
+        <tr>
+          <td className="border px-4 py-2">2</td>
+          <td className="border px-4 py-2">2023-09-13</td>
+          <td className="border px-4 py-2">Jane Smith</td>
+          <td className="border px-4 py-2">$75</td>
+          <td className="border px-4 py-2">
+            <span className="px-2 py-1 rounded bg-green-500 text-white">
+              Completed
+            </span>
+          </td>
+          <td className="border px-4 py-2">
+            <a href="#" className="text-blue-500 hover:text-blue-700 mr-2">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 20 20"
+                stroke="currentColor"
+                className="w-5 h-5"
               >
-                Date
-              </th>
-              <th
-                scope="col"
-                className="px-6 py-3 font-semibold text-3xl text-gray-900 dark:text-white text-center"
+                <path
+                  fillRule="evenodd"
+                  d="M4.293 8.293a1 1 0 0 1 1.414 0L10 12.586l4.293-4.293a1 1 0 1 1 1.414 1.414l-5 5a1 1 0 0 1-1.414 0l-5-5a1 1 0 0 1 0-1.414z"
+                  clipRule="evenodd"
+                ></path>
+              </svg>
+            </a>
+            <a href="#" className="text-red-500 hover:text-red-700">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 20 20"
+                stroke="currentColor"
+                className="w-5 h-5"
               >
-                State
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr className="bg-gray-50 dark:bg-gray-900 text-center">
-              <td className="px-6 py-4">Apple MacBook Pro 17"</td>
-              <td className="px-6 py-4">65265</td>
-              <td className="px-6 py-4">28-08-2023</td>
-              <td className="px-6 py-4">Sent</td>
-            </tr>
-            <tr className="bg-gray-50 dark:bg-gray-900 text-center">
-              <td className="px-6 py-4">Microsoft Surface Pro</td>
-              <td className="px-6 py-4">365345345</td>
-              <td className="px-6 py-4">28-08-2023</td>
-              <td className="px-6 py-4">Sent</td>
-            </tr>
-            <tr className="bg-gray-50 dark:bg-gray-900 text-center">
-              <td className="px-6 py-4">Magic Mouse 2</td>
-              <td className="px-6 py-4">3345354354</td>
-              <td className="px-6 py-4">28-08-2023</td>
-              <td className="px-6 py-4">Sent</td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
+                <path
+                  fillRule="evenodd"
+                  d="M5.293 6.293a1 1 0 0 1 1.414 0L10 10.586l3.293-3.293a1 1 0 1 1 1.414 1.414l-4 4a1 1 0 0 1-1.414 0l-4-4a1 1 0 0 1 0-1.414z"
+                  clipRule="evenodd"
+                ></path>
+              </svg>
+            </a>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</div>
     </>
-  );
+  )
 };
 
 export default Admin;
