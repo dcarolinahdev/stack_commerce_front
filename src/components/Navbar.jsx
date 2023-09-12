0508ae76166d279a541b263e7f3d0b6892b84370
Robert Michael González Núñez <71914244+ROBERTG96@users.noreply.github.com>
2023-09-12 02:22:07 -0500

    Changed images carousel

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -24,7 +24,7 @@ export default function Navbar() {
       });
 
       if (result.isConfirmed) {
-        await axios.post(apiUrl + "/auth/signup", null, headers());
+        await axios.put(apiUrl + "/auth/signout", null, headers());
         localStorage.removeItem("token");
         localStorage.removeItem("user");
         window.location.replace("/");
@@ -37,8 +37,8 @@ export default function Navbar() {
   const [options, setOptions] = useState([
     { to: "/", title: "Home" },
     { to: "/category", title: "Category" },
-    { to: "/register", title: "Regiter" },
-    { to: "/signin", title: "Sing In" },
+    { to: "/register", title: "Register" },
+    { to: "/signin", title: "Sign In" },
   ]);
   useEffect(() => {
     let user = JSON.parse(localStorage.getItem("user"));
@@ -48,20 +48,18 @@ export default function Navbar() {
         { to: "/", title: "Home" },
         { to: "/category_login", title: "Products" },
         { to: "/cart", title: "Cart" },
-
       ]);
     } else if (user?.role === 2) {
       setOptions([
         { to: "/", title: "Home" },
         { to: "/admin", title: "Admin Panel" },
-
       ]);
     } else {
       setOptions([
         { to: "/", title: "Home" },
         { to: "/category", title: "Category" },
-        { to: "/register", title: "Regiter" },
-        { to: "/signin ", title: "Sing In" },
+        { to: "/register", title: "Register" },
+        { to: "/signin ", title: "Sign In" },
       ]);
     }
   }, []);
@@ -69,44 +67,102 @@ export default function Navbar() {
   let user = JSON.parse(localStorage.getItem("user"));
   let email = user?.email;
   let photo = user?.photo;
+  let user_id = user?._id;
 
   return (
     <>
-      <nav className="bg-white border-gray-200 dark:bg-gray-900">
+      <nav className="bg-gradient-to-r from-t_background1 to-t_background3 text-white">
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
           <a className="flex items-center">
-            <img src="/assets/logo.ico" className="h-8 mr-3" alt="Flowbite Logo" />
-            <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white"> StackCommerce</span>
+            <img
+              src="/assets/logo.ico"
+              className="h-8 mr-3 rounded-full bg-white"
+              alt="Flowbite Logo "
+            />
+            <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+              {" "}
+              StackCommerce
+            </span>
           </a>
           <div className="flex items-center md:order-2">
-            <RiShoppingCartLine className="text-2xl mr-4 cursor-pointer" />
-            <button type="button" className="flex mr-3 text-sm bg-gray-800 rounded-full md:mr-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600" id="user-menu-button" aria-expanded="false" data-dropdown-toggle="user-dropdown" data-dropdown-placement="bottom">
-              <img className="w-8 h-8 rounded-full" src={photo} alt="user photo" />
+            {user_id ? (
+              <Anchor to={"/cart"}>
+                <RiShoppingCartLine className="text-2xl mr-4 cursor-pointer" />
+              </Anchor>
+            ) : (
+              <Anchor to={"/signin"}>
+                <RiShoppingCartLine className="text-2xl mr-4 cursor-pointer" />
+              </Anchor>
+            )}
+
+            <button
+              type="button"
+              className="flex mr-3 text-sm bg-gray-800 rounded-full md:mr-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600"
+              id="user-menu-button"
+              aria-expanded="false"
+              data-dropdown-toggle="user-dropdown"
+              data-dropdown-placement="bottom"
+            >
+              <img
+                className="w-8 h-8 rounded-full "
+                src={photo}
+                alt="user photo"
+              />
             </button>
             {/* <RiShoppingCartLine className="text-2xl ml-4 " /> */}
-
-
-            <div className="z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600" id="user-dropdown">
+            <div
+              className="z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600"
+              id="user-dropdown"
+            >
               <div className="px-4 py-3">
-                <span className="block text-sm text-gray-900 dark:text-white">Profile</span>
-                <span className="block text-sm  text-gray-500 truncate dark:text-gray-400">{email}</span>
+                <span className="block text-sm text-gray-900 dark:text-white">
+                  Profile
+                </span>
+                <span className="block text-sm  text-gray-500 truncate dark:text-gray-400">
+                  {email}
+                </span>
               </div>
               <ul className="py-2" aria-labelledby="user-menu-button">
-
                 <li>
-                  <Anchor onClick={signout} className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Sign out</Anchor>
+                  <Anchor
+                    onClick={signout}
+                    className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+                  >
+                    Sign out
+                  </Anchor>
                 </li>
               </ul>
             </div>
-            <button data-collapse-toggle="navbar-user" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-user" aria-expanded="false">
+            <button
+              data-collapse-toggle="navbar-user"
+              type="button"
+              className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+              aria-controls="navbar-user"
+              aria-expanded="false"
+            >
               <span className="sr-only">Open main menu</span>
-              <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15" />
+              <svg
+                className="w-5 h-5"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 17 14"
+              >
+                <path
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M1 1h15M1 7h15M1 13h15"
+                />
               </svg>
             </button>
           </div>
-          <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-user">
-            <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+          <div
+            className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
+            id="navbar-user"
+          >
+            <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border md:flex-row md:space-x-8 md:mt-0 md:border-0 ">
               <li>
                 {options?.map((option, index) => (
                   <Anchor
@@ -114,7 +170,7 @@ export default function Navbar() {
                     key={index}
                     to={option?.to}
                     className="mr-4"
-                  // className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                    // className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
                   >
                     {option?.title}
                   </Anchor>
