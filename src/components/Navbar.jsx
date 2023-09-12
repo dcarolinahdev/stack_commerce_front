435147be8fcb3c2b8cb061ab085fdd47440c6957
harold alzate <99224461+harold12a@users.noreply.github.com>
2023-09-12 12:53:51 -0500

    modified:   src/components/Navbar.jsx (#27)

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -86,53 +86,56 @@ export default function Navbar() {
           </a>
           <div className="flex items-center md:order-2">
             {user_id ? (
+              <>
               <Anchor to={"/cart"}>
                 <RiShoppingCartLine className="text-2xl mr-4 cursor-pointer" />
               </Anchor>
+                 <button
+                 type="button"
+                 className="flex mr-3 text-sm bg-gray-800 rounded-full md:mr-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600"
+                 id="user-menu-button"
+                 aria-expanded="false"
+                 data-dropdown-toggle="user-dropdown"
+                 data-dropdown-placement="bottom"
+               >
+                 <img
+                   className="w-8 h-8 rounded-full "
+                   src={photo}
+                   alt="user photo"
+                 />
+               </button>
+               <div
+                 className="z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600"
+                 id="user-dropdown"
+               >
+                
+                 <div className="px-4 py-3">
+                   <span className="block text-sm text-gray-900 dark:text-white">
+                     Profile
+                   </span>
+                   <span className="block text-sm  text-gray-500 truncate dark:text-gray-400">
+                     {email}
+                   </span>
+                 </div>
+                 <ul className="py-2" aria-labelledby="user-menu-button">
+                   <li>
+                     <Anchor
+                       onClick={signout}
+                       className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+                     >
+                       Sign out
+                     </Anchor>
+                   </li>
+                 </ul>
+               </div>
+               </>
             ) : (
               <Anchor to={"/signin"}>
                 <RiShoppingCartLine className="text-2xl mr-4 cursor-pointer" />
               </Anchor>
             )}
 
-            <button
-              type="button"
-              className="flex mr-3 text-sm bg-gray-800 rounded-full md:mr-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600"
-              id="user-menu-button"
-              aria-expanded="false"
-              data-dropdown-toggle="user-dropdown"
-              data-dropdown-placement="bottom"
-            >
-              <img
-                className="w-8 h-8 rounded-full "
-                src={photo}
-                alt="user photo"
-              />
-            </button>
-            {/* <RiShoppingCartLine className="text-2xl ml-4 " /> */}
-            <div
-              className="z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600"
-              id="user-dropdown"
-            >
-              <div className="px-4 py-3">
-                <span className="block text-sm text-gray-900 dark:text-white">
-                  Profile
-                </span>
-                <span className="block text-sm  text-gray-500 truncate dark:text-gray-400">
-                  {email}
-                </span>
-              </div>
-              <ul className="py-2" aria-labelledby="user-menu-button">
-                <li>
-                  <Anchor
-                    onClick={signout}
-                    className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
-                  >
-                    Sign out
-                  </Anchor>
-                </li>
-              </ul>
-            </div>
+         
             <button
               data-collapse-toggle="navbar-user"
               type="button"
