1602668490c74366d084e080def320d0d65f1dc7
santiagogutierre8 <santiagogutierre8@gmail.com>
2023-09-13 09:56:41 -0500

    mejorar en footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,14 +6,22 @@ import logo from "/assets/logo.ico";
 
 export default function Footer() {
   return (
-    <footer className="flex flex-col mt-[1%]   justify-center px-12 py-10 gap-6 bg-gradient-to-r from-t_background1  to-t_background3 text-t_main">
+    <footer className="flex flex-col mt-[1%]   justify-center px-12 py-10 gap-6">
+      <div className="bottom-0   hidden xl:block overflow-x-hidden    ">
+          <img
+            src="/assets/footer.png"
+            alt=""
+            className="mx-auto w-[90%]"
+            style={{ borderRadius: "0 0 140% 140%" }}
+          /></div>
+          <hr className="border-b border-[#3BE2AB] my-4 w-[75%] mx-auto mt-[2%]   " />
       <div className="flex items-center justify-between">
-        <span className="text-xl md:text-2xl font-medium">Contact us</span>
+        <span className="text-xl md:text-2xl mx-[15%] font-extrabold font-serif text-[#3BE2AF]">Contact us</span>
 
         <div className="flex flex-col justify-around items-end">
           <Anchor to={"#"} className="flex items-center">
             <img
-              className="w-10 h-10 mr-3 bg-t_main rounded-full  hidden md:block  hover:bg-t_main hover:rounded-full cursor-pointer transition-transform transform hover:scale-105 "
+              className="w-10 h-10 mr-20 bg-t_main rounded-full  hidden md:block hover:bg-t_main hover:rounded-full cursor-pointer transition-transform transform hover:scale-105 "
               src={logo}
               alt="logo"
             ></img>
@@ -21,23 +29,23 @@ export default function Footer() {
               StackCommerce
             </span>
           </Anchor>
-          <div className="flex gap-2">
+          <div className="flex gap-2 mx-[70%]">
             <Anchor>
-              <HiMail className="w-6 h-6 rounded-full p-1 bg-t_main text-t_stroke transition-transform transform hover:scale-105  " />
+              <HiMail className="w-6 h-6 rounded-full p-1 bg-t_main bg-[#3BE2AB] text-t_stroke text-[white] transition-transform transform hover:scale-105  " />
             </Anchor>
             <Anchor>
-              <BiLogoFacebook className="w-6 h-6 rounded-full p-1 bg-t_main text-t_stroke transition-transform transform hover:scale-105  " />
+              <BiLogoFacebook className="w-6 h-6 rounded-full p-1 bg-t_main bg-[#3BE2AB] text-t_stroke text-[white] transition-transform transform hover:scale-105  " />
             </Anchor>
             <Anchor>
-              <BiLogoInstagramAlt className="w-6 h-6 rounded-full p-1 bg-t_main text-t_stroke transition-transform transform hover:scale-105 " />
+              <BiLogoInstagramAlt className="w-6 h-6 rounded-full p-1 bg-t_main bg-[#3BE2AB] text-t_stroke text-[white] transition-transform transform hover:scale-105 " />
             </Anchor>
             <Anchor>
-              <RiWhatsappFill className="w-6 h-6 rounded-full p-1 bg-t_main text-t_stroke transition-transform transform hover:scale-105 " />
+              <RiWhatsappFill className="w-6 h-6 rounded-full p-1 bg-t_main bg-[#3BE2AB] text-t_stroke text-[white] transition-transform transform hover:scale-105 " />
             </Anchor>
           </div>
         </div>
       </div>
-      <span className="text-center font-medium">© 2023 StackCommerce</span>
+      <span className="text-center font-extrabold font-serif text-[#3BE2AF]">© 2023 StackCommerce</span>
     </footer>
   );
 }
